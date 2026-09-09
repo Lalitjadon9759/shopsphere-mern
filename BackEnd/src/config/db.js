4e83b91deb7845824104a54c07e451e73c5e8285
Lalitjadon9759 <lalitkrthakur2001@gmail.com>
2026-09-09 21:04:00 +0530

    variable string updated

--- BackEnd/src/config/db.js
+++ BackEnd/src/config/db.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+console.log("DATABASE_URL starts with:", process.env.DATABASE_URL?.slice(0, 20));
 
+await mongoose.connect(process.env.DATABASE_URL);
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error(
